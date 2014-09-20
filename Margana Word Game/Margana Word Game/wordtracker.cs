26b3b36fc714164dd76aeedb5ac8d1c6ad12de6f
Russell Mazonde <russell_mazonde@csoft.co.zw>
2014-09-20 21:00:15 +0200

    added a new class that creates an instance for a list and adds words that have been used as well as check if a word has been used.

---
+++ Margana Word Game/Margana Word Game/wordtracker.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Russell Chamunorwa Mazonde
+ * small class for adding used words to a list
+ * also checks if a word has been used before
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Margana_Word_Game
+{
+    class wordtracker
+    {
+        List<string> cmv_UsedWords = new List<string>(); //we use this list to keep track of words used
+
+        public wordtracker()
+        {
+
+        }
+
+        private  void _addNewWord(string _word){
+            cmv_UsedWords.Add(_word); //add 
+        }
+
+        private Boolean _checkWordUsed(string _word)
+        {
+            foreach(string cmv_check in cmv_UsedWords){
+                if (cmv_check == _word)
+                    //word exists in list
+                    return true;
+            }
+            return false;
+        }
+
+        private void _clearList()
+        {
+            cmv_UsedWords.Clear(); // empty the list
+        }
+    }
+}
